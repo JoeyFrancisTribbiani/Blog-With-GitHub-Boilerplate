82051a5a6580360b505ebd2937c42dbd8f11b779
elder frog <joeyfrancistrabbiani@outlook.com>
2020-07-28 18:22:02 +0800

    j

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "丰乳肥臀"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "南山第十一淫贼"
+email = "joeyfrancistribbiani@outlook.com"
+author_homepage = "https://www.google.com"
+description = "玉人何处教吹箫"
+key_words = ['Maverick', '那我懂你意思了', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
